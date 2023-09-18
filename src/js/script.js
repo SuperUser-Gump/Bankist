817fc3aaf3dd31ed61c90507c7e91e909a6f7cd7
Krittin Saenpakdi <gump29062@gmail.com>
2023-09-18 16:07:12 +0700

    fix: Correct smooth scrolling effect
    - Removed the "Open account" button from the smooth scrolling effect.

--- src/js/script.js
+++ src/js/script.js
@@ -45,7 +45,10 @@ btnScrollTo.addEventListener('click', function (e) {
 document.querySelector('.nav__links').addEventListener('click', function (e) {
   e.preventDefault();
 
-  if (e.target.classList.contains('nav__link')) {
+  if (
+    e.target.classList.contains('nav__link') &&
+    !e.target.classList.contains('nav__link--btn')
+  ) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
